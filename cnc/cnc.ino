04fc7071e436d97d971ffa55d2970f9fde1223e1
Artem Rapinchuk <artemr@playtika.com>
2020-04-16 23:55:34 +0300

    Integrate fileReader with arduino cnc frimware

--- cnc/cnc.ino
+++ cnc/cnc.ino
@@ -1,40 +1,133 @@
 #include "filegetter.h"
+
+#define ACC 40000
+#define MAX_SPEED 5000
+
+struct StepInfo
+{
+    long int x;
+    long int y;
+    long int z;
+    int xSpeed;
+    int ySpeed;
+    int zSpeed;
+};
+
 // global vars
 SdFile binFile;
-Display* disp;
+Display *disp;
+AccelStepper xStepper(1, 2, 5);
+AccelStepper yStepper(1, 3, 6);
+AccelStepper zStepper(1, 4, 7);
+bool req = true;
+
+// #define SERIAL
+
+#ifdef SERIAL
+    #define DEVICE Serial
+#else
+    #define DEVICE binFile
+#endif
+
+StepInfo ReadPacket();
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  // setup 
-  disp = new Display();
-  disp->showLogo();
-  FileGetter getter(disp);
+    xStepper.setMaxSpeed(MAX_SPEED);
+    yStepper.setMaxSpeed(MAX_SPEED);
+    zStepper.setMaxSpeed(MAX_SPEED);
+    xStepper.setAcceleration(ACC);
+    yStepper.setAcceleration(ACC);
+    zStepper.setAcceleration(ACC);
 
-  // getting file with display and encoder
-  getter.get(binFile);
-  getter.show(binFile);
+    // setup
+    disp = new Display();
+    disp->showLogo();
+    FileGetter getter(disp);
 
-  // work with file
-  Serial.println("file");
-  char buff[32];
-  binFile.getName(buff, 32);
-  Serial.println(buff);
-  Serial.println("file size");
-  Serial.println(binFile.fileSize());
-  Serial.println("file available");
-  Serial.println(binFile.available());
-  Serial.println("firts byte");
-  Serial.println((int)(binFile.read()));
+    // getting file with display and encoder
+    getter.get(binFile);
+    getter.show(binFile);
 
-  // any stuff
-  delete disp;
-  pinMode(LED_BUILTIN, OUTPUT);
+    // work with file
+    Serial.println("file");
+    char buff[32];
+    binFile.getName(buff, 32);
+    Serial.println(buff);
+    Serial.println("file size");
+    Serial.println(binFile.fileSize());
+    Serial.println("file available");
+    Serial.println(binFile.available());
+    Serial.println("firts byte");
+    Serial.println((int)(binFile.read()));
+
+    // any stuff
+    delete disp;
+    pinMode(LED_BUILTIN, OUTPUT);
+    // while (!Serial)
+    // {
+    //   ; // wait for serial port to connect. Needed for native USB
+    // }
 }
 
- 
 void loop()
-{  
-  analogWrite(LED_BUILTIN, (sin((millis()%1300)/100.0) + 1.0) * 127);   // turn the LED on (HIGH is the voltage level) 
+{
+    if (!req && DEVICE.available() > 0)
+    {      
+        byte mode = DEVICE.read();
+
+        StepInfo step;
+        step = ReadPacket();
+
+        if(step.xSpeed != 0 && step.x != 0)
+        {
+            xStepper.setMaxSpeed(step.xSpeed);
+            xStepper.moveTo(step.x);
+        }
+        if(step.ySpeed != 0 && step.y != 0)
+        {
+            yStepper.setMaxSpeed(step.ySpeed);
+            yStepper.moveTo(step.y);
+        }
+        if(step.zSpeed != 0  && step.z != 0)
+        {
+            zStepper.setMaxSpeed(step.zSpeed);
+            zStepper.moveTo(-1 * step.z);
+        }
+
+        req = true;
+    }
+
+    if (!zStepper.isRunning())
+    {
+        yStepper.run();
+        xStepper.run();
+    }
+    zStepper.run();
+    
+    if (req && !xStepper.isRunning() && !yStepper.isRunning())
+    {
+        req = false;
+        Serial.print('#');
+    }
+}
+
+StepInfo ReadPacket()
+{
+    byte raw[18];
+    int i = 0;
+
+    while (i != 18)
+    {
+        if (DEVICE.available() > 0)
+        {
+            raw[i] = DEVICE.read();
+            ++i;
+        }
+    }
+
+    StepInfo &buffer = (StepInfo &)raw;
+    return buffer;
 }
